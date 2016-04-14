9c9026f72fab3aadb11fe3fdd8eb43806f0cf29f
crschroeder94 <crschroeder94@hotmail.com>
2016-04-14 15:12:38 -0400

    list adapter lab stuff

--- app/src/main/java/com/maxbradley/dirtydishes/MainActivity.java
+++ app/src/main/java/com/maxbradley/dirtydishes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.maxbradley.dirtydishes;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,49 +10,186 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-//this is Christine
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.util.Date;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.maxbradley.dirtydishes.Chore.Priority;
+import com.maxbradley.dirtydishes.Chore.Status;
+
+public class MainActivity extends ListActivity {
+
+    private static final int ADD_TODO_ITEM_REQUEST = 0;
+    private static final String FILE_NAME = "TodoManagerActivityData.txt";
+    private static final String TAG = "Lab-UserInterface";
+
+    // IDs for menu items
+    private static final int MENU_DELETE = Menu.FIRST;
+    private static final int MENU_DUMP = Menu.FIRST + 1;
+
+    List_Adapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new List_Adapter(getApplicationContext());
+
+        getListView().setFooterDividersEnabled(true);
+
+        TextView footerView =  (TextView) getLayoutInflater().inflate(R.layout.footer, null);
+
+        getListView().addFooterView(footerView);
+        footerView.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                Intent intent= new Intent(MainActivity.this, AddNewChore.class);
+                startActivityForResult(intent,ADD_TODO_ITEM_REQUEST);
             }
         });
 
-        //test comment 2
-        //test comment Cameron
-        //max branch test
-        //test comment ctmyers
+        getListView().setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+
+        if (requestCode == ADD_TODO_ITEM_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Chore item = new Chore(data);
+                mAdapter.add(item);
+            }
+        }
+
+    }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mAdapter.getCount() == 0)
+            loadItems();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+        saveItems();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(Menu.NONE, MENU_DELETE, Menu.NONE, "Delete all");
+        menu.add(Menu.NONE, MENU_DUMP, Menu.NONE, "Dump to log");
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case MENU_DELETE:
+                mAdapter.clear();
+                return true;
+            case MENU_DUMP:
+                dump();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void dump() {
+
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            String data = ((Chore) mAdapter.getItem(i)).toLog();
+            Log.i(TAG,	"Item " + i + ": " + data.replace(Chore.ITEM_SEP, ","));
         }
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    // Load stored ToDoItems
+    private void loadItems() {
+        BufferedReader reader = null;
+        try {
+            FileInputStream fis = openFileInput(FILE_NAME);
+            reader = new BufferedReader(new InputStreamReader(fis));
+
+            String title = null;
+            String priority = null;
+            String status = null;
+            Date date = null;
+
+            while (null != (title = reader.readLine())) {
+                priority = reader.readLine();
+                status = reader.readLine();
+                date = Chore.FORMAT.parse(reader.readLine());
+                mAdapter.add(new Chore(title, Priority.valueOf(priority),
+                        Status.valueOf(status), date));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != reader) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void saveItems() {
+        PrintWriter writer = null;
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                    fos)));
+
+            for (int idx = 0; idx < mAdapter.getCount(); idx++) {
+
+                writer.println(mAdapter.getItem(idx));
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != writer) {
+                writer.close();
+            }
+        }
     }
 }
